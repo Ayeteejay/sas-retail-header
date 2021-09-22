3a4f5ba1f17ea88705ba2e3a0cbd6aaa10cd4c57
Adam Jones <adam@vsslagency.com>
2021-09-22 16:39:00 -0700

    Finishing first pass on header and footer

--- header/index.js
+++ header/index.js
@@ -14,10 +14,30 @@ const middleLine = document.querySelector(".middle-line");
 const bottomLine = document.querySelector(".bottom-line");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-
 mobileMenuButton.addEventListener("click",()=>{
     topLine.classList.toggle("rotate-top");
     middleLine.classList.toggle("hide-middle");
     bottomLine.classList.toggle("rotate-bottom");
     mobileMenu.classList.toggle("reveal-mobile-menu")
+})
+
+const mobileMenuLoginButton = document.querySelector(".mobile-menu-login-default");
+const mobileMenuLoginRows = document.querySelector(".mobile-menu-login-options");
+const chevronIcon = document.querySelector(".chevron");
+
+mobileMenuLoginButton.addEventListener("click",()=>{
+mobileMenuLoginRows.classList.toggle("reveal-mobile-menu-login");
+chevronIcon.classList.toggle("rotate")
+})
+
+window.addEventListener('resize',()=>{
+    console.log(window.innerWidth);
+    if(window.innerWidth > 991){
+        if(mobileMenu.classList.contains("reveal-mobile-menu")){
+            mobileMenu.classList.toggle("reveal-mobile-menu");
+            topLine.classList.toggle("rotate-top");
+            middleLine.classList.toggle("hide-middle");
+            bottomLine.classList.toggle("rotate-bottom");
+        }
+    }
 })
